b96a81d02ef3f0ee86180afcf90346f9a8e44b58
Your Name <mohammed.nyamatullah@gmail.com>
2025-10-30 10:30:55 +0000

    zap changes

--- python_app/app.py
+++ python_app/app.py
@@ -1,72 +1,5 @@
-
-# from flask import Flask, jsonify
-# from redis_client import get_redis_client
-
-# app = Flask(__name__)
-# redis_client = get_redis_client()
-
-# # Missing Exception Handling
-# @app.route('/danger')
-# def danger():
-#     result = 10 / 0  # Division by zero
-#     return jsonify({"result": result})
-
-# @app.route('/')
-# def home():
-#     return jsonify({"message": "Welcome to the Redis Counter API!"})
-
-# @app.route('/counter/increment', methods=['POST'])
-# def increment():
-#     if redis_client:
-#         value = redis_client.incr('counter')
-#     else:
-#         value = 999  # ❌ Hardcoded fallback
-#     return jsonify({"counter": value})
-
-# @app.route('/counter')
-# def get_counter():
-#     if redis_client:
-#         value = redis_client.get('counter')
-#         if value is None:
-#             value = 0
-#         else:
-#             value = int(value)
-#     else:
-#         value = 999  # ❌ Dummy fallback
-#     return jsonify({"counter": value})
-
-# # Duplicate Logic -- to be removed
-# @app.route('/counter/show', methods=['GET'])
-# def show_counter():
-#     value = redis_client.get('counter')
-#     if value is None:
-#         value = 0
-#     else:
-#         value = int(value)
-#     return jsonify({"counter": value})
-
-# # Unused Code
-# def unused_function():
-#     x = "This function is never called"
-#     return x
-
-# @app.route("/unsafe")
-# def unsafe():
-#     # ❌ Command Injection vulnerability
-#     cmd = request.args.get("cmd")
-#     output = subprocess.check_output(cmd, shell=True)  # BAD: unsanitized input
-#     return jsonify({"output": output.decode()})
-
-# @app.route("/hardcoded_secret")
-# def secret():
-#     # ❌ Hardcoded secret / credential
-#     return jsonify({"api_key": api_key})
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
-#TEst
-#testw
-
+#New test
+#modifying
 from flask import Flask, jsonify
 from redis_client import get_redis_client
 import logging
@@ -104,3 +37,19 @@ def get_counter():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+
+    # import pytest
+# from app import app, redis_client
+# import sys, os
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+# @pytest.fixture
+# def client():
+#     app.config['TESTING'] = True
+#     return app.test_client()
+
+# def test_home(client):
+#     response = client.get('/')
+#     assert response.status_code == 200
